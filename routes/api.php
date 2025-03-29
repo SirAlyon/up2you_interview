5394f6e683dd2652d6fb0ce4b27112bc9f0e3e6e
Alessio Sada <alessiosada04@gmail.com>
2025-03-29 17:47:40 +0100

    Update README.md

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,6 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\AttendeeController;
 use App\Http\Controllers\ParticipationController;
 
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
 Route::middleware('auth.apikey')->group(function () {
     Route::apiResource('events', EventController::class);
     Route::apiResource('attendees', AttendeeController::class);
